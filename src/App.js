bb1ffefb9e6ed2f72160b020a1160c4109a96c46
maurivarela002 <mauricio.varela@2innovateit.com>
2021-06-03 21:56:31 -0300

    modifique etiqueta p

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Bienvenido a Fulbito
+          Bienvenido a Fulbito...
         </p>
       </header>
     </div>
