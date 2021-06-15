0e737269be7b0f1d88df7f8f0df97622a51feb34
maurivarela002 <mauricio.varela@2innovateit.com>
2021-06-15 20:57:47 -0300

    favicon y logo

--- src/componentes/navBar/navBar.js
+++ src/componentes/navBar/navBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../navBar/navBar.css'
 import { Navbar } from 'react-bootstrap';
 
-export const NavBar = ({inicio, home}) => {
+export const NavBar = ({inicio, home, logo}) => {
     return (
       <Navbar variant="dark" bg="dark">
+      <img className="img" src={logo} alt="Logo" />
       <Navbar href="#">{inicio}</Navbar>
       <Navbar href="#">{home}</Navbar>
     </Navbar>
--- src/home.js
+++ src/home.js
@@ -1,15 +1,15 @@
 import './home.css';
 import { NavBar } from '../src/componentes/navBar/navBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import logo from '../src/assets/logo.gif'; // Tell webpack this JS file uses this image
 
 export const Home = () =>{
   const navInicio = "Fulbito";
   const navHome = "Home";
-
   return (
     <div className="homeNav">
     <NavBar 
+      logo={logo}
       inicio={navInicio}
       home={navHome}
     />
