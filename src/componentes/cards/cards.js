2cf2f80f117b030813f572db362a56a30ee1c571
maurivarela002 <mauricio.varela@2innovateit.com>
2021-06-17 21:02:17 -0300

    sum y rest items

---
+++ src/componentes/cards/cards.js
@@ -0,0 +1,16 @@
+import { React } from "react";
+import "../cards/cards.css";
+import { Card } from "react-bootstrap";
+
+export const Cards = ({ img, title, info, btn }) => {
+    return (
+        <Card border="primary">
+            <Card.Img className="imgCard" src={img} />
+            <Card.Body className="cardData">
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{info}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+ 
--- src/home.js
+++ src/home.js
@@ -1,31 +1,77 @@
 import "./home.css";
 import { React, useState } from "react";
 import { NavBar } from "../src/componentes/navBar/navBar";
+import { Cards } from "../src/componentes/cards/cards";
 import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "../src/assets/logo.gif"; // Tell webpack this JS file uses this image
+import logo from "../src/assets/logo.gif";
+import imgCard from "../src/assets/guantes.jpg";
 
 export const Home = () => {
   const [contador, setContador] = useState(0);
+  const [sumar, setSumar] = useState("");
+  const [restar, setRestar] = useState("");
 
   const navInicio = "Fulbito";
   const navHome = "Home";
 
+  const titleCard = "Guantes de Portero";
+  const infoCard = "Guantes De Golero De Fútbol Para Niño Niña";
+
   const hanleClick = () => {
-    setContador(contador + 1);
+    setContador(contador);
+  };
+
+  const swichtClick = (i) => {
+    let newResult;
+    switch (i) {
+      case "+":
+        newResult = Number(sumar) + 1;
+        break;
+      case "-":
+        newResult = Number(restar) - 1;
+        break;
+      default:
+        break;
+    }
+    setContador(newResult);
   };
   return (
     <div className="homeNav">
       <NavBar logo={logo} inicio={navInicio} home={navHome} />
       <div className="div">
-        <h4 className="texto">Toca el botón para sumar 1</h4>
-        <button
-          className="btn"
-          onClick={hanleClick}
-          type="button"
-          class="btn btn-primary"
-        >
-          {contador}
-        </button>
+        <h4 className="texto">Toca el botón para sumar 1 o restar 1</h4>
+        <div>
+          <button
+            className="btn"
+            onClick={hanleClick}
+            type="button"
+            class="btn btn-primary"
+          >
+            {contador}
+          </button>
+        </div>
+
+        <div>
+          <button
+            className="btn"
+            onClick={() => swichtClick("+")}
+            type="button"
+            class="btn btn-primary"
+          >
+            +
+          </button>
+          <button
+            className="btn"
+            onClick={() => swichtClick("-")}
+            type="button"
+            class="btn btn-primary"
+          >
+            -
+          </button>
+        </div>
+      </div>
+      <div className="cards">
+        <Cards img={imgCard} title={titleCard} info={infoCard} />
       </div>
     </div>
   );
